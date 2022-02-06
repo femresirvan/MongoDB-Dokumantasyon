9e467eef04b29f0f162602e0f26be60299e460ea
femresirvan <femresirvan@gmail.com>
2022-02-07 01:37:17 +0300

    limit counts completed usages added

--- aggregations_notes.js
+++ aggregations_notes.js
@@ -1,17 +1,41 @@
 /**
+ * ! Bazı yerleri direkt copy-paste aldım zamanım oldukça çevireceğim.
+ * ! Buradaki notlar 0'dan hiçbir şey anlatmamaktadır. Ders notu gibi bir dökümantasyon çıkartmaya çalıştım.
+ * ! Mongodb ve aggregation kullanmayan herhangi bir kimse notları okuyarak pek bir şey kazanamayabilir.
+ */
+
+/**
  * *AGGREGATION NOTLARI*
  */
 
 /**
  * *AGGREGATION NİÇİN YAPILIR*
+ * Real-time analytics
+ * Report generation with roll-ups, sums & averages
+ * Real-time dashboards
+ * Redacting data to present via views
+ * Joining data together from different collections on the "server-side"
+ * Data science, including data discovery and data wrangling
+ * Mass data analysis at scale (a la "big data")
+ * Real-time queries where deeper "server-side" data post-processing is required than provided by the MongoDB Query Language (MQL)
+ * Copying and transforming subsets of data from one collection to another
+ * Navigating relationships between records, looking for patterns
+ * Data masking to obfuscate sensitive data
+ * Performing the Transform (T) part of an Extract-Load-Transform (ELT) workload
+ * Data quality reporting and cleansing
+ * Updating a materialised view with the results of the most recent source data changes
+ * Representing data ready to be exposed via SQL/ODBC/JDBC (using MongoDB's BI Connector)
+ * Supporting machine learning frameworks for efficient data analysis (e.g. via MongoDB's Spark Connector)
+ * ...and many more
  */
 
 /**
  * *AGGREGATION BEST PRACTICES*
- * ! Her stage arasını boşluk bırakarak yazın ve stagelerin başına comment "//"
- * ! kullanın çünkü stage'lerin karışması muhtemeldir.
- * ! Her stage ayrı bir aggregate kullanılarak yazılabilir dolayısıyla
- * ! debugging için o stage'i boş bir aggregation'da çalıştırabiliriz.
+ * Her stage arasını boşluk bırakarak yazın ve stagelerin başına comment "//"
+ * kullanın çünkü stage'lerin karışması muhtemeldir.
+ * Her stage ayrı bir aggregate kullanılarak yazılabilir dolayısıyla
+ * debugging için o stage'i boş bir aggregation'da çalıştırabiliriz.
+ * Map ve reduce kullanılmamaktadır yerine yine artık aggregation kullanıyoruz.
  */
 
 /**
@@ -26,12 +50,13 @@
  * Başlangıç:
  * @match filtreleme yapar. sqldeki where sorgusuna denktir.
  * @project
+ * @addFields dökümana yeni fieldlar eklemeye veya var olanı değiştirmeye yarar. update gibi
  * @group SQL sorgusunda GROUP BY işlevinin yapıldığı stage'dir.
  * @lookup
  * @unwind
- * @sort
+ * @sort belirlenen field veya fieldlarda sıralama işlemi yapar.
  * @limit result'taki max document sayısını belirler.
- * @counts
+ * @count adı üstünde gösterilen dökümanları sayar.
  *
  * @match filtreleme yapar. sqldeki where sorgusuna denktir.
  * {$match: {<query>}}
@@ -69,24 +94,49 @@
  * {$match { awards: { $elemMatch: { award: "Turing Award", year: { $gt: 1980 } } } }}
  *
  * ? Match için dökümantasyonlar:
- * https://docs.mongodb.com/manual/reference/method/db.collection.find/
+ * https://docs.mongodb.com/manual/reference/method/db.collection.find/ (Kapsamlı <query> için)
+ * https://www.tutorialspoint.com/mongodb/mongodb_query_document.htm (Bast <query> için)
+ * https://docs.mongodb.com/manual/reference/operator/aggregation/match/#mongodb-pipeline-pipe.-match
  *
- * @project
+ * @project spesific olarak istediğimiz fieldları getirmeye yarar.
+ * concating işlemi de var meraklısı bakabilir burada veya project deydi sanırsam.
+ * Kullanımı => { $project: { "<field1>": 0, "<field2>": 0, ... } } // Return all but the specified fields
+ *
+ * !Default olarak _id : 1 dir.
+ * @addFields dökümana yeni fieldlar eklemeye veya var olanı değiştirmeye yarar. update gibi
+ * Burada çok iyi anlatılmış: https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/#mongodb-pipeline-pipe.-addFields
  *
  * @group SQL sorgusunda GROUP BY işlevinin yapıldığı stage'dir.
+ *
  *
  * @lookup
  *
  * @unwind
  *
- * @sort
+ * @sort belirlenen field veya fieldlarda sıralama işlemi yapar.
+ * Kullanım: { $sort: { <field1>: <sort order>, <field2>: <sort order> ... } }
+ * ? <sort order> kullanımı:
+ * kullanım-1: 1 Ascending artan sıralama
+ * kullanım-2: -1 Descending azalan sıralama
+ * kullanım-3: { $meta: "textScore" } Sort by the computed textScore metadata in descending order.
+ * computed textscore metadata için kaynak: https://docs.mongodb.com/manual/reference/operator/aggregation/sort/#std-label-sort-pipeline-metadata
+ *
  *
  * @limit result'taki max document sayısını belirler.
- * {$limit: 20}
+ * limit mutlaka sorttan sonra gelmelidir. sıralandıktan sonra limiti gerçekleştirmelidir.
+ * yoksa sıralanılmamış verinin ilk 20 elemanını sıralardı çok saçma olur.
+ * Kullanım: { $limit: <positive 64-bit integer> }
+ * Örnek: { $limit: 20 }
  * !burada önemli olan husus aggregation da result bir arraydir
  * !ve o array içerisinde o arrayin 20 elemanlı olması gerektiğini söylediğimizdir. Yanlış bir anlaşılma olmasın.
+ * @count adı üstünde gösterilen dökümanları sayar.
+ * ! The $count stage is equivalent to the following $group + $project sequence:
+ * db.collection.aggregate( [
+ *  { $group: { _id: null, myCount: { $sum: 1 } } },
+ *  { $project: { _id: 0 } } ] )
  */
 
 /**
- * ! Map ve reduce kullanılmamaktadır yerine yine artık aggregation kullanıyoruz.
+ * *YARDIMCI KAYNAKLAR*
+ * https://www.practical-mongodb-aggregations.com/who-this-is-for.html
  */
